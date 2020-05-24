06e7d64319b9c208dd9787534079b90a1586731e
Sanjaykumar Muthukumar <sanjay.m@dal.ca>
2020-05-24 16:02:30 -0300

    Init project

---
+++ Team20/src/test/java/org/dal/cs5308/t20/Project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dal.cs5308.t20.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
