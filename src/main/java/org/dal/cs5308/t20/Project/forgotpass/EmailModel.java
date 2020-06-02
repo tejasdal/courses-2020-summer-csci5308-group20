7270d3798b772e4e48b697ceb9a95ea7f2b6a97a
Dhruv Tarpara <tarpara.dhruv@dal.ca>
2020-06-01 22:03:49 -0300

    removed unnecessary interfaces

--- src/main/java/org/dal/cs5308/t20/Project/forgotpass/EmailModel.java
+++ src/main/java/org/dal/cs5308/t20/Project/forgotpass/EmailModel.java
@@ -1,14 +1,12 @@
 package org.dal.cs5308.t20.Project.forgotpass;
 
-public class EmailModel implements IEmailModel {
+public class EmailModel {
     public String email;
 
-    @Override
     public void setEmail(String email) {
         this.email = email;
     }
 
-    @Override
     public String getEmail() {
         return email;
     }
--- src/main/java/org/dal/cs5308/t20/Project/forgotpass/IEmailModel.java
+++
@@ -1,9 +0,0 @@
-package org.dal.cs5308.t20.Project.forgotpass;
-
-public interface IEmailModel {
-    String email = null;
-
-    void setEmail(String email);
-
-    String getEmail();
-}
