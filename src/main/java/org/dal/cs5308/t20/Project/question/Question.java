78cdfd8eed6c8907cb19e6a7a1d2e77822cdca67
Sanjaykumar Muthukumar <sanjay.m@dal.ca>
2020-06-06 22:10:17 -0300

    Fixed incorrect merge.

--- src/main/java/org/dal/cs5308/t20/Project/question/Question.java
+++ src/main/java/org/dal/cs5308/t20/Project/question/Question.java
@@ -7,36 +7,24 @@ public class Question {
 	public static final int MULTIPLE_CHOICE_CHOOSE_ONE = 2;
 	public static final int MULTIPLE_CHOICE_CHOOSE_MANY = 3;
 	public static final int FREE_TEXT = 4;
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> 7d279f7ba596c8299828ab8d59447c5cf5c32316
 	private Long id;
 	private String title;
 	private String description;
 	private Long userId;
 	private int questionType;
 	private List<QuestionAnswer> presetAnswers;
-<<<<<<< HEAD
 	private Long createdAt;
 
 	public Question(Long id, String title, String description, Long userId, int questionType, Long createdAt,
 			List<QuestionAnswer> presetAnswers) {
-=======
-	
-	public Question(Long id, String title, String description, Long userId, int questionType, List<QuestionAnswer> presetAnswers) {
->>>>>>> 7d279f7ba596c8299828ab8d59447c5cf5c32316
 		this.id = id;
 		this.title = title;
 		this.description = description;
 		this.userId = userId;
 		this.questionType = questionType;
 		this.presetAnswers = presetAnswers;
-<<<<<<< HEAD
 		this.createdAt = createdAt;
-=======
->>>>>>> 7d279f7ba596c8299828ab8d59447c5cf5c32316
 	}
 
 	public static int getNumeric() {
@@ -74,7 +62,6 @@ public class Question {
 	public int getQuestionType() {
 		return questionType;
 	}
-<<<<<<< HEAD
 
 	public List<QuestionAnswer> getPresetAnswers() {
 		return presetAnswers;
@@ -83,10 +70,5 @@ public class Question {
 	public Long getCreatedAt() {
 		return createdAt;
 	}
-=======
-	
-	public List<QuestionAnswer> getPresetAnswers() {
-		return presetAnswers;
-	}
->>>>>>> 7d279f7ba596c8299828ab8d59447c5cf5c32316
+
 }
--- src/main/java/org/dal/cs5308/t20/Project/question/QuestionAnswer.java
+++ src/main/java/org/dal/cs5308/t20/Project/question/QuestionAnswer.java
@@ -1,19 +1,12 @@
 package org.dal.cs5308.t20.Project.question;
 
 public class QuestionAnswer {
-<<<<<<< HEAD
 	private Long id;
 	private String answer;
 	private int value;
 	
 	public QuestionAnswer(Long id, String answer, int value) {
 		this.id = id;
-=======
-	private String answer;
-	private int value;
-	
-	public QuestionAnswer(String answer, int value) {
->>>>>>> 7d279f7ba596c8299828ab8d59447c5cf5c32316
 		this.answer = answer;
 		this.value = value;
 	}
@@ -26,10 +19,7 @@ public class QuestionAnswer {
 		return value;
 	}
 
-<<<<<<< HEAD
 	public Long getId() {
 		return id;
 	}
-=======
->>>>>>> 7d279f7ba596c8299828ab8d59447c5cf5c32316
 }
