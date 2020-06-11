a2a4ee37513675517dedfa88c6a489c8b4fbe1a6
Sanjaykumar Muthukumar <sanjay.m@dal.ca>
2020-06-11 13:46:28 -0300

    Wiped out repo to prepare importing Rob's code.

--- src/main/java/org/dal/cs5308/t20/Project/user/PasswordChangeFailedException.java
+++
@@ -1,8 +0,0 @@
-package org.dal.cs5308.t20.Project.user;
-
-@SuppressWarnings("serial")
-public class PasswordChangeFailedException extends Exception {
-	public PasswordChangeFailedException(String message) {
-		super(message);
-	}
-}
