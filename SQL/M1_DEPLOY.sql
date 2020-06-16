47bda37cbba24068c37080db8c1de451572f248c
Tejas Patel <tejas.patel@dal.ca>
2020-06-16 18:38:46 -0300

    Added changes to add button to CourseAdmin page and Question Manager page.

--- SQL/M1_DEPLOY.sql
+++ SQL/M1_DEPLOY.sql
@@ -39,6 +39,27 @@ CREATE TABLE SystemRole (
     FOREIGN KEY (userID) REFERENCES User(id)
 );
 
+CREATE TABLE Question(
+    ID bigint NOT NULL,
+    TITLE VARCHAR(100) NOT NULL,
+    DESCRIPTION VARCHAR(300) NOT NULL,
+    TYPE_ID INT NOT NULL,
+    USER_ID BIGINT NOT NULL,
+    CREATED_AT DATE NOT NULL,
+    PRIMARY KEY(ID),
+    FOREIGN KEY(TYPE_ID) REFERENCES QuestionType(ID),
+    FOREIGN KEY(USER_ID) REFERENCES User(ID)
+);
+
+CREATE TABLE QuestionOption(
+    ID BIGINT NOT NULL AUTO_INCREMENT,
+    QUESTION_ID BIGINT NOT NULL,
+    OPTIONS VARCHAR(200) NOT NULL,
+    VALUE INT NOT NULL,
+    PRIMARY KEY(ID),
+    FOREIGN KEY(QUESTION_ID) REFERENCES Question(ID)
+);
+
 INSERT INTO Role(role)
 VALUES
     ('Admin'),
@@ -70,3 +91,10 @@ VALUES (@adminRoleID, @adminID);
 
 SELECT * FROM Role;
 SELECT * FROM User;
+
+INSERT INTO QuestionType
+VALUES
+    (1, 'numeric'),
+    (2, 'multiple-choice-choose-one'),
+    (3, 'multiple-choice-choose-many'),
+    (4, 'free-text');
